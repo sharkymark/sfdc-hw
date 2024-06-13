1c51d1e506309a3d2a7bb27a8650275ce227ab7f
Mark Milligan <mtm20176@gmail.com>
2024-06-13 11:40:19 -0500

    chore: move contacts prompt to top, and contact addt. columns

--- accounts.py
+++ accounts.py
@@ -12,6 +12,16 @@ def main():
 
     print("\nConnected to Salesforce - first time\n")
 
+    # Get additional columns for accounts
+    account_additional_columns = input("\nEnter additional column names comma-separated for accounts besides Id, Name, Description:\n e.g., Industry, Phone, Website, TickerSymbol ")
+
+    # Ask if user wants to show contacts
+    show_contacts = input("\nShow contacts for each account? (yes/no): ").lower() == 'yes'
+
+    if show_contacts:
+        contact_additional_columns = input("\nEnter additional column names comma-separated for contacts besides Id, FirstName, LastName, Email, Title, Phone, Description:\ne.g., MobilePhone, CreatedDate, LastModifiedDate, LeadSource ")
+
+
     while True:
         try:
 
@@ -36,14 +46,10 @@ def main():
                 # Query accounts
                 query = f"SELECT Id, Name, Description"
                 
-                additional_columns = input("\nEnter additional column names comma-separated besides Id, Name, Description which are in the default query e.g., CreatedDate, Phone, Website, Type - see columns with input parameter `describe` on next query: ")
-                if additional_columns:
-                    query += ", " + additional_columns
+                if account_additional_columns:
+                    query += ", " + account_additional_columns
                 else:
                     query += " "
-
-                # Ask if user wants to show contacts
-                show_contacts = input("\nShow contacts for each account? (yes/no): ").lower() == 'yes'
 
                 query += f" FROM ACCOUNT WHERE Name LIKE '%{account_name}%'"
 
@@ -57,8 +63,8 @@ def main():
                     accounts = sf.query(query)
 
                 # Print column headers
-                if additional_columns:
-                    columns = ['Id', 'Name', 'Description'] + [column.strip() for column in additional_columns.split(', ')]
+                if account_additional_columns:
+                    columns = ['Id', 'Name', 'Description'] + [column.strip() for column in account_additional_columns.split(', ')]
                     print(*columns, sep='\t')  # Print column names
                 else:
                     print('Id', 'Name', 'Description', sep='\t')  # Print default column names
@@ -67,19 +73,32 @@ def main():
                 for account in accounts['records']:
                     print(account['Id'], account['Name'], account['Description'], *[
 
-                        account[column] for column in additional_columns.split(', ') if column.strip() != ''
+                        account[column] for column in account_additional_columns.split(', ') if column.strip() != ''
 
                     ])
 
                     # Get contacts for the account
                     if show_contacts:
-                        contact_query = f"SELECT Id, FirstName, LastName, Email, Title, Phone, Description FROM Contact WHERE AccountId = '{account['Id']}'"
+                        contact_query = f"SELECT Id, FirstName, LastName, Email, Title, Phone, Description"
+                        if contact_additional_columns:
+                            contact_query += ", " + contact_additional_columns
+                        contact_query += f" FROM Contact WHERE AccountId = '{account['Id']}'"
+
                         contacts = sf.query(contact_query)
                         print("\nContacts query: ", contact_query)
                         print("\nContacts:")
-                        print("Id", "FirstName", "LastName", "Email","Title","Phone","Description", sep='\t')
+
+                        if contact_additional_columns:
+                            contact_columns = ['Id', 'FirstName', 'LastName', 'Email', 'Title', 'Phone', 'Description'] + [column.strip() for column in contact_additional_columns.split(', ')]
+                            print(*contact_columns, sep='\t')
+                        else:
+                            print("Id", "FirstName", "LastName", "Email","Title","Phone","Description", sep='\t')
+
                         for contact in contacts['records']:
-                            print(contact['Id'], contact['FirstName'], contact['LastName'], contact['Email'], contact['Title'], contact['Phone'], contact['Description'], sep='\t')
+                            print(contact.__dict__)
+                            print(contact['Id'], contact['FirstName'], contact['LastName'], contact['Email'], contact['Title'], contact['Phone'], contact['Description'], *[
+                                contact.get(column) for column in contact_additional_columns.split(', ') if column.strip() != ''
+                            ])
 
 
 
